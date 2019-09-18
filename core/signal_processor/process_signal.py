2e7f062802dc57663dc2de2154a429b64795ac0d
Saurav <sauravg@chetu.com>
2019-09-18 23:18:58 +0530

    Started with converting signals from cloud to Hw and vice-versa. Cloud signal validation complete. Created a new class for Signal conversion.

--- core/signal_processor/process_signal.py
+++ core/signal_processor/process_signal.py
@@ -5,7 +5,10 @@
 This module provides functionality to process cloud signals, change to hardware format and vice versa.
 """
 
+from os import environ as env
+
 from helper import decrypt_signal_from_cloud as decrypt, decode_hardware_signal as decode
+import name_helper as nh
 
 
 class ProcessCloudSignal:
@@ -23,8 +26,18 @@ class ProcessCloudSignal:
         # transmit new signal to hardware.
 
     def __create_signal_for_hw(self, signal):
-        # TODO: Add code to create signal for hardware according to cloud
-        return signal
+        is_valid_signal, signal_parts = self._is_valid_signal(signal)
+        if is_valid_signal:
+            signal_for_hardware = _SignalConversion().create_hardware_signal(signal_parts)
+            return signal_for_hardware
+        return None
+
+    @staticmethod
+    def _is_valid_signal(signal):
+        signal_parts = signal.split("/")
+        if signal_parts[0] == env["CLOUD_SOURCE"] and len(signal_parts) > nh.CLOUD_SIGNAL_NO_OF_PARTS:
+            return True, signal_parts
+        return False, None
 
 
 class ProcessHardwareSignal:
@@ -49,3 +62,25 @@ class ProcessHardwareSignal:
     def __create_signal_for_cloud(self, signal):
         # TODO: Add code to create signal for cloud from hardware signal.
         return signal
+
+
+class _SignalConversion:
+    def __init__(self):
+        pass
+
+    def create_hardware_signal(self, cloud_signal_parts):
+        """
+        :param cloud_signal_parts: List of parts of signal
+        e.g. [<zone>, <entity>, <id or group>, <action>]
+        :return: command to send to hardware
+        """
+        # TODO: need to convert string data into stream of 0s & 1s for sending command to hardware.
+        return ""
+
+    def create_cloud_signal(self, hw_signal_parts):
+        """
+        :param hw_signal_parts:
+        :return:  signal to transmit to cloud.
+        e.g. <source>/<zone>/<entity>/<id or group>/<status>
+        """
+        return "%s/<zone>/<entity>/<id>/<status>" % env["CLOUD_SOURCE"]
