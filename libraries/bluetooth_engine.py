0229fea4361f8f1aa3699c9697ba2ad23acc4dcc
Saurav <sauravg@chetu.com>
2019-09-21 19:08:23 +0530

    bluetooth_engine module completed (only for signal reception.)

--- libraries/bluetooth_engine.py
+++ libraries/bluetooth_engine.py
@@ -4,3 +4,53 @@
 """
 This file provides various methods which can be used to harness the capability of Bluetooth.
 """
+
+from os import environ as env
+from time import sleep
+import bluetooth as bt
+
+import name_helper as nh
+
+
+class BluetoothClient:
+    def __init__(self):
+        self.bt_sock = bt.BluetoothSocket(bt.RFCOMM)
+        self.__output_queue = None
+
+    def set_output_queue(self, output_queue):
+        self.__output_queue = output_queue
+
+    def _put_signal_onto_queue(self, signal):
+        """
+        It accepts the raw signal string received and convert to dict format with additional info
+        and puts the formatted signal onto the queue for further processing.
+        :param signal: raw signal string or sting in form of bytes.
+        :return: None
+        """
+        signal = {
+            "message": signal.decode(),
+            "protocol": "BLUETOOTH",
+            "source_type": "Bluetooth_Device"
+        }
+        self.__output_queue.put(signal)
+
+    def _connect_to_hardware(self):
+        try:
+            self.bt_sock.conn((env["A_HC05_DEVICE_ID"], env["A_HC05_PORT"]))
+            return True
+        except bt.btcommon.BluetoothError:
+            # TODO: Log bluetooth connection error
+            return False
+
+    def receive_signal(self):
+        if self._connect_to_hardware():
+            while True:
+                try:
+                    signal_received = self.bt_sock.recv(nh.BLUETOOTH_SIGNAL_LIMIT)
+                    self._put_signal_onto_queue(signal_received)
+                    sleep(nh.BLUETOOTH_SIGNAL_WAIT_TIME)
+
+                except bt.btcommon.BluetoothError:
+                    # TODO: Log error while receiving signal over bluetooth
+                    pass
+
