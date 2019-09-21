8c20389f3163fdcb8a61a2d2acb4ccf6c2c62a11
Saurav <sauravg@chetu.com>
2019-09-21 19:46:47 +0530

    Hardware controller working started.

--- hw_controller.py
+++ hw_controller.py
@@ -5,3 +5,25 @@
 This is entry point for sensors and actuators (or bridge between Pi and Arduino).
 Module which is responsible for controlling and monitoring actuators and sensors.
 """
+
+from threading import Thread
+
+
+class _HardwareClient(Thread):
+    def __init__(self, name, mode):
+        self._name = name
+        Thread.__init__(self)
+
+    def run(self):
+        pass
+
+
+class HardwareSignal:
+    def __init__(self):
+        pass
+
+    def transmit(self, signal):
+        pass
+
+    def receive(self):
+        pass
